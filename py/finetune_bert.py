9d38a3f6c73ea215f018067da1749319e0e67119
Alexis Perrier <alexis.perrier@pm.me>
2021-01-10 12:41:27 +0100

    wip

--- py/finetune_bert.py
+++ py/finetune_bert.py
@@ -20,24 +20,22 @@ import datasets
 
 # The train and test files are samples of the original MIMIC dataset restricted to the text column
 # - train: 80000 lines
-# - test: 20000 lines
+# - valid: 20000 lines
 
 # uncomment to download the datasets
 # !wget https://dataskat.s3.eu-west-3.amazonaws.com/data/columbia/MIMIC.csv
-# !wget https://dataskat.s3.eu-west-3.amazonaws.com/data/columbia/mimic_test.txt
-# !wget https://dataskat.s3.eu-west-3.amazonaws.com/data/columbia/mimic_train.txt
 
 # script run_mlm.py
 # !wget https://raw.githubusercontent.com/huggingface/transformers/master/examples/language-modeling/run_mlm.py
 
 # set the parameters
 # TODO change for json file
-model_name = 'bert-base-uncased'
-train_filepath = "./mimic_train.txt"
-test_filepath = "./mimic_test.txt"
-max_steps = 5000 # the bigger, the longer it tales to train the model
-save_steps = 2000 # each time, a snapshot of the model is saved. Warning this can take a lot of space
-output_dir = "./results/" # where the model snapshots and the final model is saved
+model_name      = 'bert-base-uncased'
+train_filepath  = "/home/alexis/data/MIMIC_train_samp100k.txt"
+valid_filepath  = "/home/alexis/data/MIMIC_valid_samp100k.txt"
+max_steps       = 5000 # the bigger, the longer it tales to train the model
+save_steps      = 2000 # each time, a snapshot of the model is saved. Warning this can take a lot of space
+output_dir      = "./models/test_01/" # where the model snapshots and the final model is saved
 
 # execute in the
 cmd = f'''
@@ -46,12 +44,12 @@ cmd = f'''
         --max_seq_length 128 \
         --line_by_line \
         --train_file "{train_filepath}" \
-        --validation_file "{test_filepath}" \
+        --validation_file "{valid_filepath}" \
         --do_train \
         --do_eval \
         --max_steps 5000 \
-        --save_steps 1000 \
-        --output_dir "results/"
+        --save_steps 2000 \
+        --output_dir "results/test_01"
 '''
 print("execute the following command in the shell")
 print(cmd)
--- py/split_MIMIC_train_valid.py
+++ py/split_MIMIC_train_valid.py
@@ -9,12 +9,12 @@ import re
 
 # replace with your own file path
 MIMIC_PATH = "/home/alexis/data/MIMIC.csv"
-TRAIN_PATH = "/home/alexis/data/MIMIC_train_samp100k.csv"
-VALID_PATH = "/home/alexis/data/MIMIC_valid_samp100k.csv"
+TRAIN_PATH = "/home/alexis/data/MIMIC_train_samp50k.csv"
+VALID_PATH = "/home/alexis/data/MIMIC_valid_samp50k.csv"
 # train, validation ratio
 training_ratio = 0.8
 # limit to N samples for testing
-N = 100000
+N = 50000
 
 REGEX_PATTERNS = {
     'linereturns': {'regex': "[\r\n]+", 'sub_by': ' '},
@@ -33,6 +33,7 @@ REGEX_PATTERNS = {
 
 if __name__ == "__main__":
     df = pd.read_csv(MIMIC_PATH, error_bad_lines = False, low_memory=False)
+    print(f"Loaded {df.shape[0]} rows from {MIMIC_PATH}")
     '''
     The MIMIC.csv file has 11 columns and 2083176 rows
     we're only concerned about the TEXT column
@@ -42,6 +43,7 @@ if __name__ == "__main__":
     for testing purposes, only keep N samples
     '''
     if N is not None:
+        print(f"subsampling {N} notes")
         df = df.sample(n = N, random_state = 88)
 
 
@@ -53,6 +55,7 @@ if __name__ == "__main__":
         [**2120-6-2**]
         ____
     '''
+    print("Regex - clean up")
     df['TEXT'] = df.TEXT.apply(lambda  txt : re.sub(
                                         REGEX_PATTERNS['linereturns']['regex'],
                                         REGEX_PATTERNS['linereturns']['sub_by'],
@@ -92,6 +95,7 @@ if __name__ == "__main__":
     '''
     Random split
     '''
+    print("Random train valid split")
 
     train_index = df.sample(frac = training_ratio, random_state = 88).index
     valid_index = np.setdiff1d(df.index, train_index, assume_unique=True)
@@ -102,8 +106,17 @@ if __name__ == "__main__":
     '''
     save to file
     '''
+    print(f"saving {len(mimic_train)} training rows to {TRAIN_PATH}")
+    mimic_train = [txt + "\n" for txt in mimic_train ]
+    f = open(TRAIN_PATH, "w")
+    f.writelines(mimic_train)
+    f.close()
 
-
+    print(f"saving {len(mimic_valid)} validation rows to {VALID_PATH}")
+    mimic_valid = [txt + "\n" for txt in mimic_valid ]
+    f = open(VALID_PATH, "w")
+    f.writelines(mimic_valid)
+    f.close()
 
 
 
